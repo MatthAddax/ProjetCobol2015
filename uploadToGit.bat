252b42ea6a90afc68f157847f4cc363b54cd186a
MatthAddax <matth.meun@hotmail.com>
2015-12-10 18:15:04 +0100

    %Test commit avec commentaire%

--- uploadToGit.bat
+++ uploadToGit.bat
@@ -1,7 +1,7 @@
 @ECHO off
 git add .
 set /p commentaireCommit="Commentaire du commit : "
-git commit -m commentaireCommit
+git commit -m "%%%commentaireCommit%%%"
 
 git push -u origin master
 pause
